82144f8ace4b65c01117ba5dc34baf4d0a4a07e9
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2024-04-03 16:41:15 -0400

    finish mission-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { MissionListComponent } from './mission-list/mission-list.component';
 import { MissionComponent } from './mission/mission.component';
+import { MissionFilterComponent } from './mission-filter/mission-filter.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MissionListComponent,
+  },
+  {
+    path: 'mission-filter',
+    component: MissionFilterComponent,
   },
   {
     path: '**',
